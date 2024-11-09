f16e544361148208a8d2b18b9807ee81e0368e33
Jack Deeth <developer@jackdeeth.org.uk>
2024-11-09 00:28:30 +0000

    Solution for day 10 pt 1

--- 10.py
+++ 10.py
@@ -1,73 +1,78 @@
 from dataclasses import dataclass
+from enum import Enum
 import pytest
 
 
+@dataclass(frozen=True, eq=True)
+class Vec2:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __neg__(self):
+        return Vec2(-self.x, -self.y)
+
+    def __str__(self):
+        return f"({self.x:3}, {self.y:3})"
+
+
+UP = Vec2(0, -1)
+DOWN = Vec2(0, 1)
+LEFT = Vec2(-1, 0)
+RIGHT = Vec2(1, 0)
+
+
 class Sketch:
-    @dataclass(frozen=True, eq=True)
-    class Vec2:
-        x: int
-        y: int
-
-        def __add__(self, other):
-            return Sketch.Vec2(self.x + other.x, self.y + other.y)
-
-        def __sub__(self, other):
-            return self + (-other)
-
-        def __neg__(self):
-            return Sketch.Vec2(-self.x, -self.y)
-
-        def __str__(self):
-            return f"({self.x:<3}, {self.y:<3})"
-
     def __init__(self, sketch: str):
         self.nodes = {}
         for y, row in enumerate(sketch.strip().splitlines()):
             for x, char in enumerate(row):
-                self.nodes[self.Vec2(x, y)] = char
+                self.nodes[Vec2(x, y)] = char
                 if char == "S":
-                    self.start = self.Vec2(x, y)
+                    self.start = Vec2(x, y)
+
+    class Tile(Enum):
+        START = "S", (UP, DOWN, LEFT, RIGHT)
+        UP_DOWN = "|", (UP, DOWN)
+        UP_LEFT = "J", (UP, LEFT)
+        UP_RIGHT = "L", (UP, RIGHT)
+        LEFT_RIGHT = "-", (LEFT, RIGHT)
+        DOWN_LEFT = "7", (DOWN, LEFT)
+        DOWN_RIGHT = "F", (DOWN, RIGHT)
+        NONE = ".", ()
+
+        def __init__(self, char, directions):
+            self.char = char
+            self.directions = directions
 
     def path_length(self):
-        vec2 = Sketch.Vec2
-        directions = (
-            (vec2(0, 1), "|LJ"),
-            (vec2(0, -1), "|7F"),
-            (vec2(-1, 0), "-LF"),
-            (vec2(1, 0), "-J7"),
-        )
-        last_dir = vec2(0, 0)
+        last_dir = Vec2(0, 0)
         loc = self.start
-        tile = "S"
-        for direction, valid_tile in directions:
-            loc = self.start + direction
-            tile = self.nodes.get(loc, "x")
-            if tile in valid_tile:
-                last_dir = direction
-                break
-
-        visited = [loc]
+        tile = self.Tile.START
 
         result = 2
-        # while True:
-        for _ in range(20):
-            print(tile, loc)
-            for direction, valid_tile in directions:
-                tile = self.nodes.get(loc + direction, ".")
+        while True:
+            for direction in tile.directions:
                 if -direction == last_dir:
                     continue
-                if tile == "S":
+                neighbour = self.nodes.get(loc + direction, ".")
+                if neighbour == "S":
                     return result // 2
-                if tile not in valid_tile:
+                n_tile = next(t for t in self.Tile if t.char == neighbour)
+                if -direction not in n_tile.directions:
                     continue
-                result += 1
                 last_dir = direction
                 loc = loc + direction
-                if loc in visited:
-                    print(visited)
-                    raise RuntimeError("revisited location")
-                visited.append(loc)
+                tile = n_tile
+                result += 1
                 break
+            else:
+                raise RuntimeError("seem to have run out of directions")
 
 
 EXAMPLE_1 = """
@@ -94,4 +99,4 @@ def test_example_paths(sketch, length):
 
 def test_10a():
     with open("10_input.txt", encoding="utf-8") as f:
-        assert Sketch(f.read()).path_length() == 0
+        assert Sketch(f.read()).path_length() == 6909
